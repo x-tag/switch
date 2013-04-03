c5ff49e1ab16ea876db14e2e3af24ed5d651f598
Arron Schaar <aschaar@gmail.com>
2013-04-03 09:39:10 -0700

    Release 0.1.8

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,32 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    
-    concat: {
+    jshint:{
+      all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    'smush-components': {
       options: {
-        separator: '\n',
-      },
-      'x-tag-js': {
-        src: [
-          'components/document.register/src/document.register.js', 
-          'components/x-tag-core/src/core.js'
-        ],
-        dest: 'demo/x-tag-components.js'
-      
+        fileMap: {
+          js: 'demo/x-tag-components.js',
+          css: 'demo/x-tag-components.css'
+        }
       }
+    },
+    bumpup: ['component.json', 'package.json', 'xtag.json'],
+    tagrelease: {
+      file: 'package.json',
+      prefix: 'xtag-v',
+      commit: true
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-   
-  grunt.registerTask('build', ['concat:x-tag-js']);
-  
-}
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bumpup');
+  grunt.loadNpmTasks('grunt-tagrelease');
+  grunt.loadNpmTasks('grunt-smush-components');
+
+  grunt.registerTask('build', ['jshint','smush-components']);
+  grunt.registerTask('bump:patch', ['bumpup:patch', 'tagrelease']);
+
+};
